afdb43c3d2cdd87f120654a97b45727a180ce141
Sumanpanta <masterrujino47@gmail.com>
2023-12-14 17:08:47 +0545

    third commit

---
+++ section-7/18.simple-if.py
@@ -0,0 +1,49 @@
+#indentation.py 
+
+#Blocks of code
+
+#4 space for Indentation
+
+#if Else
+#while
+# for 
+
+
+'''
+if <condition>:
+    <block of code>
+
+else:
+    <block of code>
+'''    #three single quotes 
+
+## syntax
+
+# the condition has to evaluate to boolean
+# the below block can contain any python code
+
+'''
+if <condition>:
+    <block of code>
+'''
+
+## Example1
+# Check whether the number given by user is even or odd
+
+user_input = input("Enter the number: \n ~$ ")
+user_input = int(user_input)
+
+if user_input %2 == 0:              #if<condition>:   <block of code>
+    print("The number is even")
+
+if user_input %2 !=0:
+    print("The number is odd")
+    
+if user_input<0:
+    print("The number is negative")
+    
+    '''
+    This is just a line of code that dont execute like other 
+    but seen by python: this is just for documentation generation or 
+    documentation purpose
+    '''
